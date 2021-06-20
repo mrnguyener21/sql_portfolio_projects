d24397a2d60354b5841b245b44f00dd83752fd73
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-19 18:41:16 -0700

    finished initial 6 queries for cases data set

--- covid_19.sql
+++ covid_19.sql
@@ -1,8 +1,49 @@
---Succesfully Imported Covid 19 US Cases to PG Admin
-select * 
-from covid19_us_cases;
+select * from covid19_us_cases
 
--- Successfully Imported Covid 19 Deaths to PG Admin
-select *
-from covid19_us_deaths;
+-- 1. Which States contained the most amount of total cases overall
+ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
+select  "State", sum(total_cases) as sum_of_total_cases
+from covid19_us_cases
+group by "State"
+order by sum_of_total_cases desc;
+-- 2. which states contained the most amount of new cases in one day
+ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
+select "State", "Date", sum(new_cases) as sum_of_new_cases
+from covid19_us_cases
+group by "State", "Date"
+order by sum_of_new_cases desc;
+
+-- 3.  which states contains the least amount of total cases overall
+ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
+select "State", sum(total_cases) as sum_of_total_cases
+from covid19_us_cases
+group by "State"
+order by sum_of_total_cases asc;
+
+-- 4. which states contained the least amount of new cases in one day after 2021
+ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
+select "State", "Date", new_cases
+from covid19_us_cases
+where "Date" > '20201231`'and new_cases > 0
+order by new_cases asc;
+
+-- 5. what is the new cases to total cases percentage for each date in each state
+ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
+ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
+select "State", "Date", total_cases, new_cases, 
+	case
+	when new_cases = 0 then NULL
+	else (new_cases/total_cases) * 100
+	end as new_to_total_cases_percentage_ratio
+from covid19_us_cases
+
+-- 6. what is the overall new cases to total cases percentage for each state 
+ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
+ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
+select "State", sum(total_cases) sum_of_total_cases, sum(new_cases) sum_of_new_cases, 
+	sum(new_cases)/sum(total_cases)*100 new_cases_to_total_cases_percentage_ratio
+from covid19_us_cases
+group by "State"
+order by new_cases_to_total_cases_percentage_ratio desc;
+
 
