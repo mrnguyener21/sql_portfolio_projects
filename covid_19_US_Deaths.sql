6ca98dd2227a648cd1dd81aa156faf6e2ac90941
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-22 19:14:13 -0700

    created a file for the deaths data set

---
+++ covid_19_US_Deaths.sql
@@ -0,0 +1,62 @@
+
+--Which States contained the most amount of total deaths overall
+
+--which states contained the most amount of new deaths in one day
+
+
+--which states contains the least amount of total deaths overall
+
+
+--which states contained the least amount of new deaths in one day after 2021
+
+
+--what is the new deaths to total deaths percentage for each date in each state
+
+
+--what is the overall new deaths to total deaths percentage for each state 
+
+
+--what is the average total deaths per state
+
+
+--Adding Additional Year and Month column based on Date Columnn for Deeper Analysis
+
+-- 8. Which months had the most amount of new deaths in 2020
+
+-- 9. Which month had the most amount of new deaths in 2021
+
+
+-- 10. Which state had the most  new deaths in month
+
+
+-- 11. Which state had the most  new deaths in a month in 2021
+
+
+-- Which states had the most  new deaths in a month in 2020
+
+
+-- Which states had the most  new deaths in a month in 2021
+
+
+-- Which states had the least  new deaths in a month in 2020
+
+-- Which states had the least  new deaths in a month in 2021
+
+
+--What were the monthly averages for each month for each state in 2020(do not include averages that are 0)
+
+
+--What were the monthly averages for each month for each state in 2021
+
+
+--What was the monthly average for the United States in 2020(exclude months where the average is 0)
+
+
+--What was the monthly average for the United States in 2021 so far (exclude months where the average is 0)
+
+
+
+--What are the yearly averages for new deaths for each State
+
+
+
--- covid_19_US_cases.sql
+++ covid_19_US_cases.sql
@@ -1,33 +1,33 @@
 select * from covid19_us_cases
 
--- 1. Which States contained the most amount of total cases overall
+--Which States contained the most amount of total cases overall
 ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
 select  "State", sum(total_cases) as sum_of_total_cases
 from covid19_us_cases
 group by "State"
 order by sum_of_total_cases desc;
--- 2. which states contained the most amount of new cases in one day
+--which states contained the most amount of new cases in one day
 ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
 select "State", "Date", sum(new_cases) as sum_of_new_cases
 from covid19_us_cases
 group by "State", "Date"
 order by sum_of_new_cases desc;
 
--- 3.  which states contains the least amount of total cases overall
+--which states contains the least amount of total cases overall
 ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
 select "State", sum(total_cases) as sum_of_total_cases
 from covid19_us_cases
 group by "State"
 order by sum_of_total_cases asc;
 
--- 4. which states contained the least amount of new cases in one day after 2021
+--which states contained the least amount of new cases in one day after 2021
 ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
 select "State", "Date", new_cases
 from covid19_us_cases
 where "Date" > '20201231`'and new_cases > 0
 order by new_cases asc;
 
--- 5. what is the new cases to total cases percentage for each date in each state
+--what is the new cases to total cases percentage for each date in each state
 ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
 ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
 select "State", "Date", total_cases, new_cases, 
@@ -37,7 +37,7 @@ select "State", "Date", total_cases, new_cases,
 	end as new_to_total_cases_percentage_ratio
 from covid19_us_cases
 
--- 6. what is the overall new cases to total cases percentage for each state 
+--what is the overall new cases to total cases percentage for each state 
 ALTER TABLE covid19_us_cases ALTER COLUMN total_cases TYPE float8;
 ALTER TABLE covid19_us_cases ALTER COLUMN new_cases TYPE float8;
 select "State", sum(total_cases) sum_of_total_cases, sum(new_cases) sum_of_new_cases, 
@@ -46,7 +46,7 @@ from covid19_us_cases
 group by "State"
 order by new_cases_to_total_cases_percentage_ratio desc;
 
--- 7. what is the average total cases per state
+--what is the average total cases per state
 select "State", avg(total_cases) average_total_cases
 from covid19_us_cases
 group by "State"
@@ -200,3 +200,5 @@ select "Year", "State", avg(new_cases) avg_new_cases
 from covid19_us_cases
 group by "Year", "State"
 order by "State" asc, "Year" asc;
+
+
