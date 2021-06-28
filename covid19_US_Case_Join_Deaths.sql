0a6eb80889632c13718a2e73e352d7ef2f26c8f1
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-27 20:40:09 -0700

    found the death to cases percentage ratio for each month in 2020 and 2021(so far)

--- covid19_US_Case_Join_Deaths.sql
+++ covid19_US_Case_Join_Deaths.sql
@@ -33,6 +33,23 @@ order by "Year"
 
 
 --what is the monthly death to cases percentage ratio for 2020 and 2021
+with cte_cases_and_deaths as (
+    select  covid19_us_cases."State" "State", 
+            covid19_us_cases."Date" "Date", 
+            covid19_us_cases."Year" "Year",
+            covid19_us_cases."Month" "Month", 
+            covid19_us_cases.total_cases, 
+            covid19_us_cases.new_cases, 
+            covid19_us_deaths.total_deaths, 
+            covid19_us_deaths.new_deaths
+    from covid19_us_cases
+    inner join covid19_us_deaths on covid19_us_cases."ID" = covid19_us_deaths."ID"
+    order by covid19_us_cases."ID"
+)
 
+select "Month", "Year",(sum(new_deaths)/sum(new_cases)) * 100 monthly_total_deaths_to_total_cases_percentage_ratio
+from cte_cases_and_deaths
+group by "Month", "Year"
+order by "Month","Year"
 --what is the monthly death to cases percentage ratio for 2020 and 2021 for each state
 
