c2c91eb33f4795479288e163cade8bace793ac6b
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-30 20:00:14 -0700

    brainstormed initial questions for gold data set

---
+++ gold_and_silver/gold_database.sql
@@ -0,0 +1,14 @@
+--how much did the price of gold increase over the four centuries;
+
+--how much did gold go increase or decrease in price each YEAR;
+
+--what was growth rate of each year;
+
+--what was the growth rate for each century;
+
+--how much did the price of gold increase by 2020 in dollar and percentage amount;
+
+--What was the price of gold for each century;
+
+--what is the overal average price of gold;
+
