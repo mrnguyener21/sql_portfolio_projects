f0813191f19c0a1e1e817f111a53f6f8018cc7ef
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-30 20:21:20 -0700

    found the average price of gold for each century

--- gold_and_silver/gold_database.sql
+++ gold_and_silver/gold_database.sql
@@ -1,16 +1,21 @@
 --how much did the price of gold increase over the four centuries;
 select max(gold_price) - min(gold_price) total_increase_in_gold_price
-from gold
+from gold;
 
---how much did gold go increase or decrease in price each YEAR;
+--how much did gold go increase or decrease in price each year;
+
 
 --what was growth rate of each year;
 
+
 --what was the growth rate for each century;
+
 
 --how much did the price of gold increase by 2020 in dollar and percentage amount;
 
+
 --What was the price of gold for each century;
+
 
 --what is the overal average price of gold;
 
