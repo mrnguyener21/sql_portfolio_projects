d93da6f50a2feb8c456cb3a9af5dd4af95f610d3
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-30 20:50:48 -0700

    found the inflation rate of gold for the entire period

--- gold_and_silver/gold_database.sql
+++ gold_and_silver/gold_database.sql
@@ -1,5 +1,8 @@
---how much did the price of gold increase over the four centuries;
-select max(gold_price) - min(gold_price) total_increase_in_gold_price
+--how much did the price of gold increase by 2020 along with the inflation rate;
+select  min(gold_price) starting_gold_price,
+        max(gold_price) current_gold_price,
+        max(gold_price) - min(gold_price) total_increase_in_gold_price,
+        concat(round((((max(gold_price) - min(gold_price)) / min(gold_price)) * 100):: numeric,2), '%') gold_price_inflation_rate
 from gold;
 
 --how much did gold go increase or decrease in price each year;
@@ -14,8 +17,11 @@ from gold;
 --how much did the price of gold increase by 2020 in dollar and percentage amount;
 
 
---What was the price of gold for each century;
-
+--What was the average price of gold for each century;
+select "century", avg(gold_price) average_gold_price
+from gold
+group by "century"
+order by "century";
 
 --what is the overal average price of gold;
 
