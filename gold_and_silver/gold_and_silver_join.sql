39712028050a1042f590b071caced34aa8e744be
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-07-05 21:26:49 -0700

    successfully joined gold and silver data set

--- gold_and_silver/gold_and_silver_join.sql
+++ gold_and_silver/gold_and_silver_join.sql
@@ -1,1 +1,6 @@
-select * from silver;
+select  gold.century,
+        gold."year",
+        gold.gold_price,
+        silver.silver_price
+from gold
+left join silver on gold."year"::numeric = silver."year"::numeric;
